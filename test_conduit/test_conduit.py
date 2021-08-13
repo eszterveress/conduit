60ebaa0c1838adbd5fe8986ceaa03174d5c6fe7c
Veress Eszter Abigel <vereszter@gmail.com>
2021-08-13 18:49:53 +0200

    update12

--- test_conduit/test_conduit.py
+++ test_conduit/test_conduit.py
@@ -113,7 +113,7 @@ def test_data_list():
 def test_pagination():
     browser = setup_env()
     conduit_registration(browser)
-    second_page = browser.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/div[1]/div[2]/div/div/nav/ul/li[2]/a')
+    second_page = webdriver_wait_xpath(browser, '//*[@id="app"]/div/div[2]/div/div[1]/div[2]/div/div/nav/ul/li[2]/a')
     second_page.click()
     # Ellenőrizzük, hogy a 2. oldalra kattintás után az az oldal válik-e aktívvá
     active_page = webdriver_wait_xpath(browser, '//*[@class="page-item active"]')
